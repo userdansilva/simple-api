8b3f0ec72e9d9d45167aa33d1c7179a6861d0fec
Daniel Silva <daniel.sousasilva97@outlook.com>
2024-12-27 06:46:41 -0300

    first commit

---
+++ src/test/java/com/catalogar/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
